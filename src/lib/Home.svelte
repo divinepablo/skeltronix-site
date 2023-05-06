2c2e891a99d9b4e69a72009802a58577c1e66ab8
skelton codes <87882202+skeltoncodez@users.noreply.github.com>
2023-05-06 13:09:18 -0400

    crumbled to peer pressure and changed the site

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,7 +1,15 @@
 <div class="flex flex-col justify-center items-center h-screen">
     <h1 class="font-sans text-white text-4xl mb-4">skeltronix.xyz</h1>
     <div class="flex flex-row">
-        <a href="#/bio" class="rounded-xl bg-blue-600 border border-black bg-opacity-90 w-[150px] h-12 text-xl text-center flex items-center justify-center font-semibold shadow-lg hover:shadow-2xl hover:shadow-black transition-all duration-300 mr-4 shadow-black">Bio</a>
-        <a href="https://forums.skeltronix.xyz" class="rounded-xl bg-blue-600 border border-black bg-opacity-90 w-[150px] h-12 text-xl text-center flex items-center justify-center font-semibold shadow-lg hover:shadow-2xl hover:shadow-black transition-all duration-300  shadow-black">Forums</a>
+        <a
+            href="#/bio"
+            class="rounded-md bg-indigo-800 border border-black bg-opacity-90 w-[150px] h-12 text-xl text-center flex items-center justify-center font-semibold shadow-md hover:shadow-lg hover:shadow-black transition-all duration-300 mr-4 shadow-black"
+            >Bio</a
+        >
+        <a
+            href="https://forums.skeltronix.xyz"
+            class="rounded-md bg-indigo-800 border border-black bg-opacity-90 w-[150px] h-12 text-xl text-center flex items-center justify-center font-semibold shadow-md hover:shadow-lg hover:shadow-black transition-all duration-300 shadow-black"
+            >Forums</a
+        >
     </div>
 </div>
